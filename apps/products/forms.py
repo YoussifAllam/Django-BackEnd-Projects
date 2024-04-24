89e665f6f9605e7b046cccb87b9dfa5c700a110f
YoussifAllam <96921160+YoussifAllam@users.noreply.github.com>
2024-04-24 21:22:34 +0200

    Delete apps/products directory

--- apps/products/forms.py
+++
@@ -1,6 +0,0 @@
-from django import forms
-from django.forms import inlineformset_factory
-from .models import Final_Product, Weights_for_Final_Product, grinding_for_Final_Product, Benefits_for_Final_Product
-
-
-

fcaaed47898c6d939001b2b7dae7b67a42d47659
YoussifAllam <96921160+YoussifAllam@users.noreply.github.com>
2024-04-24 21:20:48 +0200

    Add files via upload

---
+++ apps/products/forms.py
@@ -0,0 +1,6 @@
+from django import forms
+from django.forms import inlineformset_factory
+from .models import Final_Product, Weights_for_Final_Product, grinding_for_Final_Product, Benefits_for_Final_Product
+
+
+

6818e70b28c7378f37e3587682450bd8dd72950c
YoussifAllam <96921160+YoussifAllam@users.noreply.github.com>
2024-05-29 21:28:57 +0300

    Add files via upload

--- SpeakingFingers-BackEnd-/YOLO/views.py
+++ SpeakingFingers-BackEnd-/YOLO/views.py
@@ -1,15 +1,21 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from rest_framework.permissions import IsAuthenticated
+from django.conf import settings
+# from ultralytics import YOLO
+# import torch
+
+# Set device to CPU
 
 class HandTrackingAPI(APIView):
-    # permission_classes = [IsAuthenticated]
+    permission_classes = [IsAuthenticated]
     def post(self, request):
-        
-        
-        return Response({ 
+        # device = torch.device('cpu')
+        l = torch.cuda.is_available()
+
+        return Response({
                          'Status:':'Success',
                          'sign_language_numbers': 1
                          })
-    
+
 # Create your views here.
